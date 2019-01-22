b481176bc9634ce6c0246bedce8a10f0978e5a53
carlosrbatalha <carlosrbatalha@gmail.com>
2019-01-22 22:29:10 +0000

    added the popup and the function in js but still missing code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 
 
 var form = document.getElementById('survey-form');
+var nameResult = document.getElementById('')
 
 
 form.onsubmit = function (e) {
 
 	e.preventDefault ();
+	var nameResult = document.getElementById('');
 	console.log (form.name.value);
+ 
+
+
+
 	form.reset();
+
+
+
 };
+
+
+function myFunction(){
+    if (form.name.value == "")
+    	return false;
+    if (form.email.value == "")
+    	return false;
+
+    else
+    	return location.href="#popup1";
+}
+
+
